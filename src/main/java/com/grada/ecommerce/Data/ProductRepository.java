3923f431d9bc31f3af491aa065e6e796e2347c8a
Rishabh Gupta <@coderrishabh>
2018-04-28 02:28:49 +0530

    Added find product by product id

--- src/main/java/com/grada/ecommerce/Data/ProductRepository.java
+++ src/main/java/com/grada/ecommerce/Data/ProductRepository.java
@@ -5,11 +5,13 @@ import com.grada.ecommerce.Models.Product.ShortProduct;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Collection;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>
 {
     //Collection<Product> findByTitleLike(@Param("title") String title);
     Collection<ShortProduct> findByTitleLike(String title);
+    ShortProduct findProductByProductid(@Param("productid") String productid);
     //Collection<ShortProduct> findAllByTitleLike(@Param("title") String title);
 
     //Product findProductById(@Param("id") Long id);
