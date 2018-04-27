015b79d58be1acf04ec03bf6278643532f63016e
Rishabh Gupta <@coderrishabh>
2018-04-27 17:32:56 +0530

    Merge branch 'master' of github.com:Grada-ecommerce/Grada

---
+++ src/main/java/com/grada/ecommerce/Controllers/Api/AdminRestController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grada.ecommerce.Controllers.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Rishabh Gupta
+ */
+
+// Under Build
+@RequestMapping("api/admin")
+@RestController
+public class AdminRestController {
+    @RequestMapping("/")
+    public Iterable<> getDataApis()
+   {  Iterable<> 
+        return 
+    }
+}
