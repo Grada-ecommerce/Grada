435afb38b03028b220f118be44fdeddcd78263f8
gurudhanush pochineni <gurudhanush@outlook.com>
2018-04-28 01:04:49 +0530

    Updated

--- src/main/java/com/grada/ecommerce/Controllers/SellerController.java
+++ src/main/java/com/grada/ecommerce/Controllers/SellerController.java
@@ -19,4 +19,5 @@ public class SellerController
         return modelAndView;
     }
 
+
 }
--- src/main/java/com/grada/ecommerce/Services/CartService.java
+++ src/main/java/com/grada/ecommerce/Services/CartService.java
@@ -1,4 +1,45 @@
 package com.grada.ecommerce.Services;
 
-public class CartService {
+import com.grada.ecommerce.Models.Customer.Customer;
+import com.grada.ecommerce.Models.Product.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService
+{
+
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    ProductService productService;
+
+    public void AddProduct(Long productid, Long customerid)
+    {
+        Product tempProduct = productService.findProductByID(productid);
+
+        if(tempProduct == null)
+            return;
+
+        Customer customer = customerService.FindCustomerByID(customerid);
+        //customer.wishList.
+        customer.cart.productList.add(tempProduct);
+
+    }
+
+    public void DeleteProduct(Long productid, Long customerid)
+    {
+        Product tempProduct = productService.findProductByID(productid);
+
+        if(tempProduct == null)
+            return;
+
+        Customer customer = customerService.FindCustomerByID(customerid);
+
+        if(customer.cart.productList.contains(tempProduct))
+            customer.cart.productList.remove(tempProduct);
+        return;
+    }
+
 }
