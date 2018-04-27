b7632d05134efb1839d65e238e7c77f7fe825c65
Rishabh Gupta <@coderrishabh>
2018-04-27 17:30:34 +0530

    Merge branch 'master' of github.com:Grada-ecommerce/Grada

---
+++ src/main/java/com/grada/ecommerce/Models/Mainpageitems.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grada.ecommerce.Models;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Rishabh Gupta
+ */
+public class Mainpageitems {
+
+    public Mainpageitems(String Title, HashSet<Product> products) {
+        this.Title = Title;
+        this.products = products;
+    }
+    
+    private String Title;
+    private HashSet<Product> products;
+    
+}
